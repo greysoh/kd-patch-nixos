e69774c199ac3eb22aa5d5ce23ff6e8163e5c8ca
zero <rosascript@gmail.com>
2024-04-02 18:32:56 -0400

    devshell

--- flake.nix
+++ flake.nix
@@ -10,9 +10,28 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    lib = nixpkgs.lib // home-manager.lib;
+    systems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      });
+  in {
+    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
+      specialArgs = {inherit inputs;};
       modules = [
         ./root.nix
         inputs.home-manager.nixosModules.default
