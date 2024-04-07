fc5d3b800714b110e7ede08c288b86060c1f7f30
zero <rosascript@gmail.com>
2024-04-06 23:32:22 -0400

    add: hyprland keybindings

--- users/programs/hyprland.nix
+++ users/programs/hyprland.nix
@@ -10,6 +10,10 @@
     sleep 1
     ${pkgs.swww}/bin/swww img ${./wallpaper.jpg} &
   '';
+  waybarRestart = pkgs.writeShellScriptBin "waybarrestart" ''
+    killall waybar
+    ${pkgs.waybar}/bin/waybar &
+  '';
 in {
   wayland.windowManager.hyprland = {
     # allow home-manager to configure hyprland
@@ -19,6 +23,19 @@ in {
     settings = {
       exec-once = ''${startupScript}/bin/start'';
       general.layout = "master";
+      "$mainMod" = "SUPER";
+      "$terminal" = "wezterm";
+
+      bind = [
+        ''$mainMod, Return, exec, $terminal''
+        ''$mainMod, Tab, focusmonitor, +1''
+        ''$mainMod CTRL, P, exec, ${waybarRestart}/bin/waybarrestart''
+        ''$mainMod SHIFT, Tab, focusmonitor, -1''
+        ''CTRL ALT, S, exec, ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" ~/Pictures/Screenshots/$(date +'%s_grim.png')''
+        '', XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_SINK@ 5%-''
+        '', XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_SINK@ 5%+''
+        '', XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle''
+      ];
     };
   };
 }
