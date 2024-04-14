b2af429d7067d753043a160f17c557e802acca85
Greyson <greyson@hofers.cloud>
2024-04-13 23:51:44 -0400

    chore: Makes Nix GNOME configuration more declarative.

--- home/programs/gnome.nix
+++ home/programs/gnome.nix
@@ -3,19 +3,70 @@
   lib,
   ...
 }: {
-  dconf = {
-    enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-  };
   gtk = {
     enable = true;
+
     iconTheme = {
       name = "Zafiro-icons-Dark";
       package = pkgs.zafiro-icons;
     };
+
     theme = {
       name = "Materia-dark";
       package = pkgs.materia-theme;
     };
+
+    cursorTheme = {
+      name = "Graphite-dark-cursors";
+      package = pkgs.graphite-cursors;
+    };
   };
+
+  dconf = {
+    enable = true;
+
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+
+        enabled-extensions = [
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+          "unite@hardpixel.eu"
+        ];
+      };
+
+      "org/gnome/shell/extensions/user-theme" = {
+        name = "Materia-dark";
+      };
+
+      "org/gnome/shell/extensions/unite" = {
+        desktop-name-text = "Desktop";
+        greyscale-tray-icons = true;
+        hide-activities-button = "always";
+        hide-window-titlebars = "always";
+        show-window-buttons = "always";
+        show-window-title = "tiled";
+        window-buttons-placement = "first";
+        window-buttons-theme = "arc";
+      };
+
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+
+      "org/gnome/desktop/background" = {
+        picture-uri = "file:///${./wallpaper.jpg}";
+        picture-uri-dark = "file:///${./wallpaper.jpg}";
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    # Gnome extensions
+    gnomeExtensions.unite
+    gnomeExtensions.user-themes
+
+    # Theme
+    materia-theme
+  ];
 }
